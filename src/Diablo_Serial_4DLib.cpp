f0c098ce859fafddf300cefef1a960be09424d34
Juniel Cruz <cruz.juniel@gmail.com>
2022-08-31 14:45:38 +0800

    Removed flush calls in constructors

--- src/Diablo_Serial_4DLib.cpp
+++ src/Diablo_Serial_4DLib.cpp
@@ -11,44 +11,28 @@
 #endif
 
 Diablo_Serial_4DLib::Diablo_Serial_4DLib(Stream * virtualPort, void (*setBaudRateHndl)(unsigned long)) { 
-    _virtualPort = virtualPort; 
-    setBaudRateExternal = setBaudRateHndl;
-    setBaudRateInternal = &Diablo_Serial_4DLib::exSetBaudRateHndl;
-    unknownSerial = true;
-#if !defined(ARDUINO_ARCH_SAMD) || (ARDUINO_ARCH_SAM)
-	//Only done for non-SAMD/SAM architectures
-	_virtualPort->flush();
-#endif
+  _virtualPort = virtualPort; 
+  setBaudRateExternal = setBaudRateHndl;
+  setBaudRateInternal = &Diablo_Serial_4DLib::exSetBaudRateHndl;
+  unknownSerial = true;
 }
 
 Diablo_Serial_4DLib::Diablo_Serial_4DLib(HardwareSerial * serial) { 
-    _virtualPort = (Stream *)serial; 
-    setBaudRateInternal = &Diablo_Serial_4DLib::hwSetBaudRateHndl;
-#if !defined(ARDUINO_ARCH_SAMD) || (ARDUINO_ARCH_SAM)
-	//Only done for non-SAMD/SAM architectures
-	_virtualPort->flush();
-#endif
+  _virtualPort = (Stream *)serial; 
+  setBaudRateInternal = &Diablo_Serial_4DLib::hwSetBaudRateHndl;
 }
 
 #ifdef SoftwareSerial_h		
 Diablo_Serial_4DLib::Diablo_Serial_4DLib(SoftwareSerial * serial) { 
-    _virtualPort = (Stream *)serial; 
-    setBaudRateInternal = &Diablo_Serial_4DLib::swSetBaudRateHndl;
-#if !defined(ARDUINO_ARCH_SAMD) || (ARDUINO_ARCH_SAM)
-	//Only done for non-SAMD/SAM architectures
-	_virtualPort->flush();
-#endif
+  _virtualPort = (Stream *)serial; 
+  setBaudRateInternal = &Diablo_Serial_4DLib::swSetBaudRateHndl;
 }
 #endif
 
 #ifdef AltSoftSerial_h
 Diablo_Serial_4DLib::Diablo_Serial_4DLib(AltSoftSerial * serial) { 
-    _virtualPort = (Stream *)serial; 
-    setBaudRateInternal = &Diablo_Serial_4DLib::alSetBaudRateHndl;
-#if !defined(ARDUINO_ARCH_SAMD) || (ARDUINO_ARCH_SAM)
-	//Only done for non-SAMD/SAM architectures
-	_virtualPort->flush();
-#endif
+  _virtualPort = (Stream *)serial; 
+  setBaudRateInternal = &Diablo_Serial_4DLib::alSetBaudRateHndl;
 }
 #endif
 
