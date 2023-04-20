65b48aada2e09789161ba6fa3e2357aaea70bd86
Juniel Cruz <juniel@4dsystems.com.au>
2023-04-20 17:14:58 +0800

    Copied supporting H and CPP files from Workshop4

---
+++ src/Diablo_PrintDisk.h
@@ -0,0 +1,5 @@
+
+void PrintDisk(Diablo_Serial_4DLib Display, word hndl, long offset, word msglen, word msgid);
+
+void PrintDiskUnicode(Diablo_Serial_4DLib Display, word hndl, long offset, word msglen, word msgid);
+
